569e3762dc035fde58c4a47e35bdd182ee298c3b
Maxime Delvaux <94120181+m-delvaux@users.noreply.github.com>
2022-04-02 15:38:32 -0400

    Update config.py

--- jhub/update_config.py
+++ jhub/update_config.py
@@ -1,34 +1,39 @@
-google_config = lambda callback_url, client_id, client_secret: [
+google_config = lambda env_var: [
     f"from oauthenticator.google import LocalGoogleOAuthenticator",
-    f"c.LocalGoogleOAuthenticator.oauth_callback_url = '{callback_url}'",
-    f"c.LocalGoogleOAuthenticator.client_id = '{client_id}'",
-    f"c.LocalGoogleOAuthenticator.client_secret = '{client_secret}'",
+    f"c.LocalGoogleOAuthenticator.oauth_callback_url = '{env_var['jhub_callback_url']}'",
+    f"c.LocalGoogleOAuthenticator.client_id = '{env_var['jhub_client_id']}'",
+    f"c.LocalGoogleOAuthenticator.client_secret = '{env_var['jhub_client_secret']}'",
     "c.JupyterHub.authenticator_class = LocalGoogleOAuthenticator",
     f"c.LocalAuthenticator.create_system_users = True",
     f"c.LocalAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '\"\"', '--disabled-password', '--force-badname']"
                                                                ]
 
-auth0_config = lambda callback_url, client_id, client_secret: [
-    f"from oauthenticator.auth0 import Auth0OAuthenticator",
-    f"c.Auth0OAuthenticator.oauth_callback_url = '{callback_url}'",
-    f"c.Auth0OAuthenticator.client_id = '{client_id}'",
-    f"c.Auth0OAuthenticator.client_secret = '{client_secret}'",    
-    f"c.Auth0OAuthenticator.scope = ['openid', 'email']",
-    f"c.JupyterHub.authenticator_class = Auth0OAuthenticator",
-    f"c.LocalAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '\"\"', '--disabled-password', '--force-badname']",
-    f"c.LocalAuthenticator.create_system_users = True"    
-    ]
-
-github_config = lambda callback_url, client_id, client_secret: [
+github_config = lambda env_var: [
     f"from oauthenticator.github import GitHubOAuthenticator",
-    f"c.GitHubOAuthenticator.oauth_callback_url = '{callback_url}'",
-    f"c.GitHubOAuthenticator.client_id = '{client_id}'",
-    f"c.GitHubOAuthenticator.client_secret = '{client_secret}'",    
+    f"c.GitHubOAuthenticator.oauth_callback_url = '{env_var['jhub_callback_url']}'",
+    f"c.GitHubOAuthenticator.client_id = '{env_var['jhub_client_id']}'",
+    f"c.GitHubOAuthenticator.client_secret = '{env_var['jhub_client_secret']}'",    
     f"c.JupyterHub.authenticator_class = GitHubOAuthenticator",
     f"c.LocalAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '\"\"', '--disabled-password', '--force-badname']",
     f"c.LocalAuthenticator.create_system_users = True"
     
 ]
+
+def auth0_config(env_var):
+    assert "auth0_subdomain" in env_var.keys(), "Missing 'JHUB_AUTH0_SUBDOMAIN' environment variable'"
+    return [
+        f"from oauthenticator.auth0 import Auth0OAuthenticator",
+        f"c.Auth0OAuthenticator.auth0_subdomain = '{env_var['jhub_auth0_subdomain']}'",
+        f"c.Auth0OAuthenticator.oauth_callback_url = '{env_var['jhub_callback_url']}'",
+        f"c.Auth0OAuthenticator.client_id = '{env_var['jhub_client_id']}'",
+        f"c.Auth0OAuthenticator.client_secret = '{env_var['jhub_client_secret']}'",    
+        f"c.Auth0OAuthenticator.scope = ['openid', 'email']",
+        f"c.JupyterHub.authenticator_class = Auth0OAuthenticator",
+        f"c.LocalAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '\"\"', '--disabled-password', '--force-badname']",
+        f"c.LocalAuthenticator.create_system_users = True"    
+    ]
+
+
 
 providers_config = {
     'google': google_config,
@@ -48,8 +53,7 @@ if("jhub_oauth_provider" in env_var.keys()):
     
     jhub_config = open("/git/jhub/jupyterhub_raw.py","r").read()
     
-    callback_url, client_id, client_secret = env_var["jhub_callback_url"], env_var["jhub_client_id"], env_var["jhub_client_secret"]
-    addl_config = providers_config[env_var["jhub_oauth_provider"].lower()](callback_url, client_id, client_secret)
+    addl_config = providers_config[env_var["jhub_oauth_provider"].lower()](env_var)
 
     with open("/srv/jupyterhub/jupyterhub_config.py", "w") as fw:
         fw.write(jhub_config+"\n")
