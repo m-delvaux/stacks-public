94a3bc6172f5cc5cd4ffe0b6113b1648e7b45bca
Maxime Delvaux <94120181+m-delvaux@users.noreply.github.com>
2022-09-25 18:15:56 -0400

    Delete superset directory

--- superset/docker-setup/requirements-local.txt
+++
@@ -1,7 +0,0 @@
-flask-oauth
-flask_oauthlib
-Authlib
-superset
-pathlib2
-contextlib2
-retry
--- superset/run.sh
+++
@@ -1,3 +0,0 @@
-python /git/superset/update_config.py;
-touch /app/docker && rm -rf /app/docker/*;
-cp -rf /git/superset/docker-setup/* /app/docker && chmod +x /app/docker/*.sh;
