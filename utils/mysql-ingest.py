1713202bb83d6bf379182c8d1c84245600428e05
Maxime Delvaux <94120181+m-delvaux@users.noreply.github.com>
2022-09-25 23:21:52 -0400

    Create mysql-ingest.py

---
+++ utils/mysql-ingest.py
@@ -0,0 +1,14 @@
+import os, pandas as pd, pymysql, sys
+from sqlalchemy import create_engine
+
+file, name = sys.argv[1], sys.argv[2]
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+                       .format(user = os.getenv('MYSQL_USER'),
+                               pw = os.getenv('MYSQL_PASSWORD'),
+                               host=os.getenv('MYSQL_HOST'),
+                               db=os.getenv('MYSQL_DATABASE')
+                              )
+                      )
+df = pd.read_csv(file, dtype=str)
+df.to_sql(con=engine, name=name, if_exists='replace')
