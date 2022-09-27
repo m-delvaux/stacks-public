afd4cdfd3c261d90c970e2d5bb4a71b4b354f3a3
Maxime Delvaux <94120181+m-delvaux@users.noreply.github.com>
2022-09-26 20:32:21 -0400

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 superset_oauth_key = os.getenv('GOOGLE_OAUTH_KEY')
 superset_oauth_secret = os.getenv('GOOGLE_OAUTH_SECRET')
 
-print(os.env)
+print(os.environ)
 
 ROW_LIMIT = 5000
 SUPERSET_WORKERS = 4
